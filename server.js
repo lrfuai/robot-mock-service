48850064db86dbc8264c0574d34b78abaf48f088
Alejandro Cena <alejandro@dev.LRFUAI>
2019-04-06 11:04:03 -0300

    Added Args option

--- server.js
+++ server.js
@@ -3,36 +3,37 @@ const BotCore = require('bot-control-core');
 const manifest = require('./manifest');
 
 const {server,messages,browser} = BotCore(manifest);
-server();
 
 messages.on(messages.Events.SERVER_COMPONENTS_LOAD_COMPLETE, ({port}) => {
   console.log('Server Started at port ' + port)
 });
 
-browser.getComponentById('ultrasonic-front').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+server();
+
+browser.getComponentById('ultrasonic-front').on(messages.Events.COMPONENT_REACHED,({id,action,options,responder}) => {
+  responder.ok(id);
 });
 
-browser.getComponentById('bumper-front-left').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+browser.getComponentById('bumper-front-left').on(messages.Events.COMPONENT_REACHED,({id,action,options,responder}) => {
+  responder.ok(id);
 });
 
-browser.getComponentById('bumper-front-right').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+browser.getComponentById('bumper-front-right').on(messages.Events.COMPONENT_REACHED,({id,action,options,responder}) => {
+  responder.ok(id);
 });
 
-browser.getComponentById('motor-left').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+browser.getComponentById('motor-left').on(messages.Events.COMPONENT_REACHED,({id,action,options,args,responder}) => {
+  responder.ok(id);
 });
 
-browser.getComponentById('motor-right').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+browser.getComponentById('motor-right').on(messages.Events.COMPONENT_REACHED,({id,action,options,responder}) => {
+  responder.ok(id);
 });
 
-browser.getComponentById('led-front-left').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+browser.getComponentById('led-front-left').on(messages.Events.COMPONENT_REACHED,({id,action,options,responder}) => {
+  responder.error(id);
 });
 
-browser.getComponentById('led-front-right').on(messages.Events.COMPONENT_REACHED,({id,responder}) => {
-  responder.ok(id)
+browser.getComponentById('led-front-right').on(messages.Events.COMPONENT_REACHED,({id,action,options,responder}) => {
+  responder.invalid(id);
 });
